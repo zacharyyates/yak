576dab3e18047254240a526969ea12b956ceecdc
Zachary Yates <zachary.yates@gmail.com>
2012-10-09 18:20:47 -0700

    added basic extension methods and tests

---
+++ Source/Yak/Yak.Test/ExtensionsTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Yak.Test
+{
+    [TestClass]
+    public class ExtensionsTest
+    {
+        [TestMethod]
+        public void IsNull()
+        {
+            object nullObj = null;
+            Assert.IsTrue(nullObj.IsNull());
+
+            object notNullObj = new object();
+            Assert.IsFalse(notNullObj.IsNull());
+        }
+
+        [TestMethod]
+        public void IsNullOrEmpty()
+        {
+            string nullStr = null;
+            Assert.IsTrue(nullStr.IsNullOrEmpty());
+
+            string emptyStr = string.Empty;
+            Assert.IsTrue(emptyStr.IsNullOrEmpty());
+
+            string notNullOrEmptyStr = "not null or empty";
+            Assert.IsFalse(notNullOrEmptyStr.IsNullOrEmpty());
+        }
+
+        [TestMethod]
+        public void IsNullOrWhiteSpace()
+        {
+            string nullStr = null;
+            Assert.IsTrue(nullStr.IsNullOrWhiteSpace());
+
+            string emptyStr = string.Empty;
+            Assert.IsTrue(emptyStr.IsNullOrWhiteSpace());
+
+            string whitespaceStr = "\n\t     ";
+            Assert.IsTrue(whitespaceStr.IsNullOrWhiteSpace());
+
+            string notNullOrWhiteSpaceStr = "not null or whitespace";
+            Assert.IsFalse(notNullOrWhiteSpaceStr.IsNullOrWhiteSpace());
+        }
+
+        [TestMethod]
+        public void ThrowIfNull()
+        {
+            object nullObj = null;
+            try
+            {
+                nullObj.ThrowIfNull();
+                Assert.Fail();
+            }
+            catch (ArgumentNullException) 
+            { 
+                // catch the exception to make the test pass
+            }
+
+            try
+            {
+                nullObj.ThrowIfNull("nullObj");
+                Assert.Fail();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Assert.AreEqual(ane.ParamName, "nullObj");
+            }
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrEmpty()
+        {
+            string nullStr = null;
+            try
+            {
+                nullStr.ThrowIfNullOrEmpty();
+                Assert.Fail();
+            }
+            catch (ArgumentException)
+            {
+                // catch the exception to make the test pass
+            }
+
+            try
+            {
+                nullStr.ThrowIfNullOrEmpty("nullStr");
+                Assert.Fail();
+            }
+            catch (ArgumentException ae)
+            {
+                Assert.AreEqual("nullStr", ae.ParamName);
+            }
+        }
+
+        [TestMethod]
+        public void ThrowIfNullOrWhiteSpace()
+        {
+            string whitespaceStr = "   \t\n";
+            try
+            {
+                whitespaceStr.ThrowIfNullOrWhiteSpace();
+                Assert.Fail();
+            }
+            catch (ArgumentException)
+            {
+                // catch the exception to make the test pass
+            }
+
+            try
+            {
+                whitespaceStr.ThrowIfNullOrWhiteSpace("whitespaceStr");
+                Assert.Fail();
+            }
+            catch (ArgumentException ae)
+            {
+                Assert.AreEqual("whitespaceStr", ae.ParamName);
+            }
+        }
+    }
+}
---
+++ Source/Yak/Yak/Extensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Yak
+{
+    public static class Extensions
+    {
+        public static bool IsNull<T>(this T obj)
+        {
+            return obj == null;
+        }
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+        public static bool IsNullOrWhiteSpace(this string str)
+        {
+            return string.IsNullOrWhiteSpace(str);
+        }
+
+        #region String
+
+        public static string FormatWith(this string format, params object[] args)
+        {
+            return string.Format(format, args);
+        }
+        
+        // todo: add an overload that uses culture info
+
+        #endregion
+
+        #region Throw Helpers
+
+        public static void ThrowIfNull<T>(this T obj)
+        {
+            if (obj.IsNull()) throw new ArgumentNullException();
+        }
+        public static void ThrowIfNull<T>(this T obj, string paramName)
+        {
+            if (obj.IsNull()) throw new ArgumentNullException(paramName);
+        }
+
+        public static void ThrowIfNullOrEmpty(this string str)
+        {
+            if (str.IsNullOrEmpty()) throw new ArgumentException();
+        }
+        public static void ThrowIfNullOrEmpty(this string str, string paramName)
+        {
+            if (str.IsNullOrEmpty())       
+                throw new ArgumentException("{0} is null or empty.".FormatWith(paramName), paramName);
+        }
+
+        public static void ThrowIfNullOrWhiteSpace(this string str)
+        {
+            if (str.IsNullOrWhiteSpace()) throw new ArgumentException();
+        }
+        public static void ThrowIfNullOrWhiteSpace(this string str, string paramName)
+        {
+            if (str.IsNullOrWhiteSpace())
+                throw new ArgumentException("{0} is null or whitespace.".FormatWith(paramName), paramName);
+        }
+
+        #endregion
+    }
+}
