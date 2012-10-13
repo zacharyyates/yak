2bdca1983c4b4536babfcf0a727a7d7719076d8c
Zachary Yates <zachary.yates@gmail.com>
2012-10-12 18:16:52 -0700

    added EncodingExtensions + first lines of JQuery impl over HtmlAgilityPack and Fizzler

--- Source/Yak/Yak.Web/HtmlCompactorModule.cs
+++ Source/Yak/Yak.Web/HtmlCompactorModule.cs
@@ -1,116 +1,113 @@
-using System;
 using System.Html;
 using System.IO;
 using System.Text;
-using System.Web;
 
-// adapted from: http://madskristensen.net/post/A-whitespace-removal-HTTP-module-for-ASPNET-20.aspx
-
-/// <summary>
-/// Removes whitespace from the html response.
-/// </summary>
-public class HtmlCompactorModule : IHttpModule
+namespace System.Web
 {
-    #region IHttpModule Members
-
-    // this is left empty because apparently unsubscribing from application events is pointless:
-    // http://stackoverflow.com/questions/3424438/how-to-dispose-ihttpmodule-correctly
-    void IHttpModule.Dispose() { }
-
-    void IHttpModule.Init(HttpApplication application)
+    /// <summary>
+    /// Removes whitespace from an html response.
+    /// </summary>
+    // adapted from: http://madskristensen.net/post/A-whitespace-removal-HTTP-module-for-ASPNET-20.aspx
+    public class HtmlCompactorModule : IHttpModule
     {
-        application.BeginRequest += context_BeginRequest;
-    }
+        #region IHttpModule Members
 
-    #endregion
+        // this is left empty because apparently unsubscribing from application events is pointless:
+        // http://stackoverflow.com/questions/3424438/how-to-dispose-ihttpmodule-correctly
+        void IHttpModule.Dispose() { }
 
-    void context_BeginRequest(object sender, EventArgs e)
-    {
-        var app = sender as HttpApplication;
-        if (app.Request.RawUrl.Contains(".html"))
+        void IHttpModule.Init(HttpApplication application)
         {
-            app.Response.Filter = new HtmlCompactorStream(app.Response.Filter);
-        }
-    }
-
-    #region Stream filter
-
-    class HtmlCompactorStream : Stream
-    {
-        public HtmlCompactorStream(Stream sink)
-        {
-            _sink = sink;
-        }
-
-        Stream _sink;
-        
-        #region Delegated members
-
-        public override bool CanRead
-        {
-            get { return true; }
-        }
-
-        public override bool CanSeek
-        {
-            get { return true; }
-        }
-
-        public override bool CanWrite
-        {
-            get { return true; }
-        }
-
-        public override void Flush()
-        {
-            _sink.Flush();
-        }
-
-        public override long Length
-        {
-            get { return 0; }
-        }
-
-        long _position;
-        public override long Position
-        {
-            get { return _position; }
-            set { _position = value; }
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            return _sink.Read(buffer, offset, count);
-        }
-
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            return _sink.Seek(offset, origin);
-        }
-
-        public override void SetLength(long value)
-        {
-            _sink.SetLength(value);
-        }
-
-        public override void Close()
-        {
-            _sink.Close();
+            application.BeginRequest += context_BeginRequest;
         }
 
         #endregion
 
-        public override void Write(byte[] buffer, int offset, int count)
+        void context_BeginRequest(object sender, EventArgs e)
         {
-            var data = new byte[count];
-            Buffer.BlockCopy(buffer, offset, data, 0, count);
-            var html = Encoding.UTF8.GetString(buffer);
-
-            html = HtmlCompactor.Compact(html);
-
-            var outdata = Encoding.UTF8.GetBytes(html);
-            _sink.Write(outdata, 0, outdata.GetLength(0));
+            var app = sender as HttpApplication;
+            if (app.Request.RawUrl.Contains(".html"))
+            {
+                app.Response.Filter = new HtmlCompactorStream(app.Response.Filter);
+            }
         }
+
+        #region Stream filter
+
+        class HtmlCompactorStream : Stream
+        {
+            Stream m_Sink;
+            
+            public HtmlCompactorStream(Stream sink)
+            {
+                m_Sink = sink;
+            }
+
+            #region Delegated members
+
+            public override bool CanRead
+            {
+                get { return true; }
+            }
+
+            public override bool CanSeek
+            {
+                get { return true; }
+            }
+
+            public override bool CanWrite
+            {
+                get { return true; }
+            }
+
+            public override void Flush()
+            {
+                m_Sink.Flush();
+            }
+
+            public override long Length
+            {
+                get { return 0; }
+            }
+
+            public override long Position { get; set; }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                return m_Sink.Read(buffer, offset, count);
+            }
+
+            public override long Seek(long offset, SeekOrigin origin)
+            {
+                return m_Sink.Seek(offset, origin);
+            }
+
+            public override void SetLength(long value)
+            {
+                m_Sink.SetLength(value);
+            }
+
+            public override void Close()
+            {
+                m_Sink.Close();
+            }
+
+            #endregion
+
+            public override void Write(byte[] buffer, int offset, int count)
+            {
+                var encoding = buffer.DetectEncoding();
+
+                var data = new byte[count];
+                Buffer.BlockCopy(buffer, offset, data, 0, count);
+
+                var html = encoding.GetString(buffer);
+                html = HtmlCompactor.Compact(html);
+
+                var outdata = encoding.GetBytes(html);
+                m_Sink.Write(outdata, 0, outdata.GetLength(0));
+            }
+        }
+        #endregion
     }
-    #endregion
 }
---
+++ Source/Yak/Yak.Web/JQuery/JQuery.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HtmlAgilityPack;
+
+namespace System.Web.JQuery
+{
+    public class JQuery : IEnumerable<JQuery>
+    {
+        HtmlDocument m_Document;
+        HtmlNode m_FirstNode;
+        HtmlNodeCollection m_Nodes;
+
+        public JQuery LoadHtml(string html)
+        {
+            m_Document = new HtmlDocument();
+            m_Document.LoadHtml(html);
+            m_FirstNode = m_Document.DocumentNode;
+            
+            return this;
+        }
+
+        public JQuery this[string selector]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        void ThrowIfDocumentIsNull()
+        {
+            if (m_Document.IsNull() || m_Document.DocumentNode.IsNull())
+                throw new InvalidOperationException("Document is not loaded.");
+        }
+
+        public string Html()
+        {
+            ThrowIfDocumentIsNull();
+
+            if (!m_FirstNode.IsNull())
+                return m_FirstNode.InnerHtml;
+            else
+                return null;
+        }
+
+        public string Attr(string name)
+        {
+            name.ThrowIfNullOrEmpty("name");
+            ThrowIfDocumentIsNull();
+
+            if (!m_FirstNode.IsNull())
+            {
+                var attrib = m_FirstNode.Attributes[name];
+                if (!attrib.IsNull())
+                    return attrib.Value;
+            }
+
+            return null;
+        }
+
+        #region IEnumerable Implementation
+
+        public IEnumerator<JQuery> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
